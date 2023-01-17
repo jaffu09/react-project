2362ecab95d8884b64ea084a95f210fa8aa995cb
jafar <mdjafaralam21@gmail.com>
2023-01-17 22:06:46 +0530

    first commit

---
+++ 01-birthday-reminder/Birthday-project/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+const  App = ()=> {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} People birthdays today</h3>
+        <List people={people} />
+        <button onClick={()=>setPeople([])}>Set Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ 04-accordion/accordion/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+
+function App() {
+   const [question , setQuestions ]  = useState(data);
+  return (
+    <main>
+      <div className='container'>
+        <h3>Question and answers abut login</h3>
+        <section className='info'>
+          {
+            question.map((question)=>{
+              return (
+                <SingleQuestion key={question.id} {...question} />
+              )
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
